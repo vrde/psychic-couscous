497669374bce3ccce0ce6244348409b17e5543b6
vrde <agranzot@gmail.com>
2019-05-09 18:10:12 +0200

    friend

--- get_geth.sh
+++ get_geth.sh
@@ -5,19 +5,15 @@
 
 set -e
 
+VERSION="1.8.27"
+COMMIT="4bcc0a37"
 PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
-echo "Settings: platform ${PLATFORM}, architecture ${ARCH}, version ${VERSION}"
-
-curl -s https://api.github.com/repos/ethereum/go-ethereum/releases/latest
-VERSION=$(curl -s https://api.github.com/repos/ethereum/go-ethereum/releases/latest | python -c "import sys, json; print(json.load(sys.stdin)['tag_name'])")
-echo "Settings: platform ${PLATFORM}, architecture ${ARCH}, version ${VERSION}"
-curl -s https://api.github.com/repos/ethereum/go-ethereum/commits/${VERSION}
-COMMIT=$(curl -s https://api.github.com/repos/ethereum/go-ethereum/commits/${VERSION} | python -c "import sys, json; print(json.load(sys.stdin)['sha'])")
-NAME="geth-${PLATFORM}-amd64-${VERSION:1}-${COMMIT:0:8}"
+NAME="geth-${PLATFORM}-amd64-${VERSION}-${COMMIT}"
 DOWNLOAD_URL="https://gethstore.blob.core.windows.net/builds/${NAME}.tar.gz"
-echo "Download ${DOWNLOAD_URL}"
 TMP_FILE=$(mktemp)
+
+echo "Downloading ${DOWNLOAD_URL}"
 curl --fail ${DOWNLOAD_URL} | tar -Oxzf - ${NAME}/geth > ${TMP_FILE}
 mv ${TMP_FILE} ${HOMEDIR}/geth
 chmod +x ${HOMEDIR}/geth
