3dbc93bfd96be8fa4857681d0ff577109fd87a9d
vrde <agranzot@gmail.com>
2019-05-09 15:51:05 +0200

    old

--- get_geth.sh
+++ get_geth.sh
@@ -9,8 +9,10 @@ PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 echo "Settings: platform ${PLATFORM}, architecture ${ARCH}, version ${VERSION}"
 
+curl -s https://api.github.com/repos/ethereum/go-ethereum/releases/latest
 VERSION=$(curl -s https://api.github.com/repos/ethereum/go-ethereum/releases/latest | python -c "import sys, json; print(json.load(sys.stdin)['tag_name'])")
 echo "Settings: platform ${PLATFORM}, architecture ${ARCH}, version ${VERSION}"
+curl -s https://api.github.com/repos/ethereum/go-ethereum/commits/${VERSION}
 COMMIT=$(curl -s https://api.github.com/repos/ethereum/go-ethereum/commits/${VERSION} | python -c "import sys, json; print(json.load(sys.stdin)['sha'])")
 NAME="geth-${PLATFORM}-amd64-${VERSION:1}-${COMMIT:0:8}"
 DOWNLOAD_URL="https://gethstore.blob.core.windows.net/builds/${NAME}.tar.gz"
